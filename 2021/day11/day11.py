5f567ee179a85f35948a12ac0d680d1e93c7e69e
Alejandro Frias <alejandro@myriad.com>
2021-12-11 10:26:11 -0800

    day11

--- 2021/day11/day11.py
+++ 2021/day11/day11.py
@@ -1,43 +1,31 @@
+import sys
 import re
 import numpy as np
 
-def parse_line(line):
-    points = re.match(r"^(\d+),(\d+) -> (\d+),(\d+)$", line).groups()
-    points = list(map(int, points))
-    x1, y1, x2, y2 = points
-    return (x1, y1), (x2, y2)
 
+def flash(grid):
+    grid += 1
+    max_y, max_x = grid.shape
+    while np.where(grid == 10)[0].size > 0:
+        flash_points = set(zip(*np.where(grid == 10)))
+        while flash_points:
+            y, x  = flash_points.pop()
+            grid[max(0, y-1):min(y+2, max_y),max(0, x-1):min(x+2, max_x)] += 1
+            flash_points.update(zip(*np.where(grid == 10)))
+    grid *= grid < 10
 
 def main():
     with open("input") as f:
-        lines = [line for line in f.read().strip().split("\n")]
-    lines = list(map(parse_line, lines))
+        grid = np.array([list(map(int, row)) for row in f.read().strip().split("\n")])
 
-    max_x = max_y = 0
-    for ((x1, y1), (x2, y2)) in lines:
-        if x1 > max_x:
-            max_x = x1
-        if x2 > max_x:
-            max_x = x2
-        if y1 > max_y:
-            max_y = y1
-        if y2 > max_y:
-            max_y = y2
-
-    print(max_x, max_y)
-    grid = np.zeros((max_x + 1, max_y + 1))
-
-    for ((x1, y1), (x2, y2)) in lines:
-        identity = np.identity(min(abs(x1 - x2), abs(y1 - y2))+ 1)
-        if (x1 < x2 and y1 > y2) or (x1 > x2 and y1 < y2):
-            identity = np.fliplr(identity)
-        x1, x2 = sorted((x1, x2))
-        y1, y2 = sorted((y1, y2))
-        grid[y1:y2 + 1,x1:x2 + 1] += identity
-
-    print(np.shape(np.where(grid > 1))[1])
-
-
+    flash_count = 0
+    iteration = 0
+    max_y, max_x = grid.shape
+    while(flash_count < (max_y * max_x)):
+        iteration += 1
+        flash(grid)
+        flash_count = np.where(grid == 0)[0].size
+        print(iteration, flash_count)
 
 
 if __name__ == "__main__":
